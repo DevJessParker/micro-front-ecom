131dcf2b6f60aaafca8480ca368e6e76dc2476ec
DevJessParker <jess.k.dempsey@gmail.com>
2021-11-30 14:46:14 -0500

    initial commit

---
+++ packages/container/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Marketing from './components/Marketing.js';
+
+export default () => {
+  return <div>
+    <h1>App Proof of Life</h1>
+    <hr />
+    <Marketing />
+  </div>
+}
