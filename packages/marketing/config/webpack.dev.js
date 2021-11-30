879223d015cc79e0ecd94a9433712aadb13844e1
DevJessParker <jess.k.dempsey@gmail.com>
2021-11-30 17:40:25 -0500

    fixed broken access path for marketing bootstrap

--- packages/marketing/config/webpack.dev.js
+++ packages/marketing/config/webpack.dev.js
@@ -19,7 +19,7 @@ const devConfig = {
       name: 'marketing',
       filename: 'remoteEntry.js',
       exposes: {
-        './Marketing': './src/bootstrap.js',
+        './Marketing': './src/bootstrap',
       },
       shared: packageJSON.dependencies,
     }),
