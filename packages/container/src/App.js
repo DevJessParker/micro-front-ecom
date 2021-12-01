5365a3cf140eb8291f9cb3f9299fc5eca9ebef16
DevJessParker <jess.k.dempsey@gmail.com>
2021-12-01 10:55:47 -0500

    fixed collisions in class names between apps

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom'
+import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
+
 import Marketing from './components/Marketing.js';
 import Header from './components/Header.js'
+
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'co',
+})
 
 export default () => {
   return (
   <BrowserRouter>
-    <div>
-      <Header />
-      <Marketing />
-    </div>
+    <StylesProvider generateClassName={generateClassName}>
+      <div>
+        <Header />
+        <Marketing />
+      </div>
+    </StylesProvider>
   </BrowserRouter>
   )
 }
