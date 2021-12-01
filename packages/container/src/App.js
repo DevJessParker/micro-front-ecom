87253a8addcb368d70b53a2f357a83b4c04c34f7
DevJessParker <jess.k.dempsey@gmail.com>
2021-12-01 10:04:37 -0500

    Added header and router

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom;'
 import Marketing from './components/Marketing.js';
+import Header from './components/Header.js'
 
 export default () => {
-  return <div>
-    <h1>App Proof of Life</h1>
-    <hr />
-    <Marketing />
-  </div>
+  return (
+  <BrowserRouter>
+    <div>
+      <Header />
+      <Marketing />
+    </div>
+  </BrowserRouter>
+  )
 }
